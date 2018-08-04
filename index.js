fb68281ac37be34347eb72d609956c82082299bc
cereceres <jesus.edelcereceres@gmail.com>
2018-08-04 12:13:13 -0600

    the test are ready

---
+++ index.js
@@ -0,0 +1,46 @@
+const AWS = require('aws-sdk');
+const lambdaDefault = new AWS.Lambda();
+module.exports = (lambdaParams, lambda = lambdaDefault) => {
+    const {
+        FunctionName,
+        InvocationType = 'RequestResponse',
+        Qualifier = '$LATEST',
+        LogType = 'Tail'
+    } = lambdaParams;
+    const options = {
+        FunctionName,
+        InvocationType,
+        LogType,
+        Qualifier
+    };
+
+    return (req, res, next) => {
+        const {
+            method,
+            params,
+            query,
+            headers,
+            body
+        } = req;
+
+        options.Payload = JSON.stringify({
+            method,
+            params,
+            query,
+            headers,
+            body
+        });
+        return lambda
+            .invoke(options)
+            .promise()
+            .then((data) => {
+                console.log('data ', data);
+                if (data.FunctionError) return next(data.LogResult);
+
+                res
+                    .status(data.StatusCode)
+                    .send(data.Payload);
+            })
+            .catch(next);
+    };
+};
---
+++ test.js
@@ -0,0 +1,164 @@
+const getMiddleware = require('./index');
+const assert = require('assert');
+
+describe('test to get middleware', () => {
+    it('should return a express middleware', (done) => {
+        const params = {
+            FunctionName: 'FunctionName',
+            InvocationType : 'InvocationType',
+            Qualifier: 'Qualifier',
+            LogType: 'LogType'
+        };
+        const lambda = {
+            invoke:(options) => {
+                assert(options.Payload === JSON.stringify({
+                    method : 'GET',
+                    params: 'params',
+                    query: 'query',
+                    headers: 'headers',
+                    body: 'body'
+                }));
+
+                assert(params.FunctionName === 'FunctionName');
+                assert(params.InvocationType === 'InvocationType');
+                assert(params.Qualifier === 'Qualifier');
+                assert(params.LogType === 'LogType');
+                return {
+                    promise:() => Promise.resolve({
+                        Payload:'response',
+                        StatusCode: 'StatusCode'
+                    })
+                };
+            }
+        };
+        const middleware = getMiddleware(params, lambda);
+        const req = {
+            method : 'GET',
+            params: 'params',
+            query: 'query',
+            headers: 'headers',
+            body: 'body'
+        };
+        let called = false;
+        const res = {
+            status:(status) => {
+                assert(status === 'StatusCode');
+                called = true;
+                return res;
+            },
+            send: (send) => {
+                assert(send === 'response');
+                if (called) done();
+            }
+        };
+        middleware(req, res, (err) => assert(err));
+    });
+
+    it('should return a express middleware', (done) => {
+        const params = {
+            FunctionName: 'FunctionName',
+            InvocationType : 'InvocationType',
+            Qualifier: 'Qualifier',
+            LogType: 'LogType'
+        };
+        const lambda = {
+            invoke:(options) => {
+                assert(options.Payload === JSON.stringify({
+                    method : 'GET',
+                    params: 'params',
+                    query: 'query',
+                    headers: 'headers',
+                    body: 'body'
+                }));
+
+                assert(params.FunctionName === 'FunctionName');
+                assert(params.InvocationType === 'InvocationType');
+                assert(params.Qualifier === 'Qualifier');
+                assert(params.LogType === 'LogType');
+                return {
+                    promise:() => Promise.reject('error')
+                };
+            }
+        };
+        const middleware = getMiddleware(params, lambda);
+        const req = {
+            method : 'GET',
+            params: 'params',
+            query: 'query',
+            headers: 'headers',
+            body: 'body'
+        };
+        let called = false;
+        const res = {
+            status:(status) => {
+                assert(status === 'StatusCode');
+                called = true;
+                return res;
+            },
+            send: (send) => {
+                assert(send === 'response');
+                if (called) done();
+            }
+        };
+        middleware(req, res, (err) => {
+            assert(err === 'error');
+            done();
+        });
+    });
+
+    it('should return a express middleware', (done) => {
+        const params = {
+            FunctionName: 'FunctionName',
+            InvocationType : 'InvocationType',
+            Qualifier: 'Qualifier',
+            LogType: 'LogType'
+        };
+        const lambda = {
+            invoke:(options) => {
+                assert(options.Payload === JSON.stringify({
+                    method : 'GET',
+                    params: 'params',
+                    query: 'query',
+                    headers: 'headers',
+                    body: 'body'
+                }));
+
+                assert(params.FunctionName === 'FunctionName');
+                assert(params.InvocationType === 'InvocationType');
+                assert(params.Qualifier === 'Qualifier');
+                assert(params.LogType === 'LogType');
+                return {
+                    promise:() => Promise.resolve({
+                        FunctionError:'FunctionError',
+                        LogResult: 'LogResults'
+                    })
+                };
+            }
+        };
+        const middleware = getMiddleware(params, lambda);
+        const req = {
+            method : 'GET',
+            params: 'params',
+            query: 'query',
+            headers: 'headers',
+            body: 'body'
+        };
+        let called = false;
+        const res = {
+            status:(status) => {
+                assert(status === 'StatusCode');
+                called = true;
+                return res;
+            },
+            send: (send) => {
+                assert(send === 'response');
+                if (called) done();
+            }
+        };
+        middleware(req, res, (err) => {
+            console.log('err ', err);
+            assert(err === 'LogResults');
+            done();
+        });
+    });
+});
